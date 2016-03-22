8baf6565873bab4f3880c6607b28d34a4ebf6ce5
skydarkness47 <yonathanaguirre@live.com>
2016-03-22 19:59:51 -0300

    #aprendiendo a programar en php estructuras y variablers.

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title> Clase 2 programacion 3	</title>
+</head>
+<body>
+		Jonathan Aguirre
+		<br>
+	<?php
+
+
+	echo "<h1>hola</h1>"; //DENTRO DE LOS COMENTARIOS PHP SE PUEDE ESCRIBIR HTML
+
+	var $pepe; // LAS VARIABLES EN PHP NO SON DE NINGUN TIPO. TODAS LAS VARIABLES LLEVAN SIGNO PESOS 
+
+
+	?>
+
+
+</body>
+</html>
