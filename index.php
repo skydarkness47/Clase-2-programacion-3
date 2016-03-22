284042a75c03892561f0fbd5ca1776acd41139c8
skydarkness47 <yonathanaguirre@live.com>
2016-03-22 20:17:35 -0300

    #Uso de if/foreach

--- index.php
+++ index.php
@@ -4,14 +4,41 @@
 </head>
 <body>
 		Jonathan Aguirre
-		<br>
+	
 	<?php
 
 
-	echo "<h1>hola</h1>"; //DENTRO DE LOS COMENTARIOS PHP SE PUEDE ESCRIBIR HTML
+	echo "<h1> Jonathan  </h1>";    //DENTRO DE LOS COMENTARIOS PHP SE PUEDE ESCRIBIR HTML
+			$nombre = "Jonathan";
+	  
+	   echo $nombre;          //LAS VARIABLES EN PHP NO SON DE NINGUN TIPO. TODAS LAS VARIABLES LLEVAN SIGNO PESOS 
+		
+		echo "<h1>" .$nombre. "</h1>"; // CONCATENO HTML CON PHP
 
-	var $pepe; // LAS VARIABLES EN PHP NO SON DE NINGUN TIPO. TODAS LAS VARIABLES LLEVAN SIGNO PESOS 
+		$numero = 33;
 
+
+		if($numero < 18) // USO DE IF
+		{
+
+		print("Es menor de edad");
+
+		}
+		else
+		{
+
+			print("es mayor de edad");
+		}
+
+		$arraynuevo = array(1,2,3,5);
+
+		$arraynuevo[] = 66;
+		$arraynuevo[2] = 666;
+		foreach ($arraynuevo as $num) 
+
+		{
+			echo "<br>" .$num ;
+		}
 
 	?>
 
